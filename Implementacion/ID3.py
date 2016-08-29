d48fa6e359f466c56efb0d5fd26aeb780f2d7e1c
Damian <Damian@192.168.15.2>
2016-08-29 18:15:12 -0300

    --

--- Implementacion/ID3.py
+++ Implementacion/ID3.py
@@ -3,31 +3,29 @@ Metodos de Aprendizaje Automatico
 Facultad de Ingenieria (UdelaR), 2016
 Practico 2, Ejercicio 6
 
-@author:
+@author: 
     Erguiz, Daniel        4.554.025-3
     Mechulam, Nicolas     4.933.997-7
     Salvia, Damian        4.452.120-0
-
-@summary:
+    
+@summary: 
    Modela el algoritmo ID3.
    Realiza su entrenamiento y verificacion.
 
-@attention:
-    Notaciones
-    - ejemplos : {(d1,pol1),...,(dM,polM)}, donde d:Dato y pol:Polaridad
-    - ejemplo  : {a1:v1,...,aN:vN}, donde a:Atributo y v:Valor
-    - polaridad: "+" o "-"
+@attention: 
+    ejemplos  : [{a1:v1,...,aN:vN}], donde a:Atributo y v:Valor  
 '''
 
 from Tree import Nodo
 from math import log
 from itertools import groupby
 from collections import defaultdict
+from copy import deepcopy
 
 class ID3:
     '''
-    @summary:
-    '''
+    @summary:    
+    ''' 
     # Dataset
     examples = [
             {"Cielo":"Sol"   , "Temperatura":"Alta" , "Humedad":"Alta"  , "Viento":"Debil"  ,"JugarTenis":'-'},
@@ -47,59 +45,63 @@ class ID3:
         ]
     values   = defaultdict(set)
     ejemplos = []
-
+    
     def __init__(self,ejemplos=None):
         '''
-        Instancia la clase, indicando opcionalmente un dataset de ejemplos
-        @param ejemplos: [({d11:v11,...,d1N:v1N},p1),...,({dM1:vM1,...,dMN:vMN},pM)]
+        Instancia la clase, indicando opcionalmente un dataset de ejemplos  
         '''
-
+        
         # Si dan ejemplos nuevos, instanciarlos
         if ejemplos: self.examples = ejemplos
-
+        
         # Extraer los valores de atributos para cada dato
-        for ejemplo in self.examples:
-            for atributo, valor in ejemplo.items():
-                self.values[atributo].add(valor)
-
+        for example in self.examples:
+            for atributo, valor in example.items():
+                self.values[atributo].add(valor) 
+        
     def execute(self,target_attribute,attributes):
         '''
         Modela el algoritmo ID3
-        @param target_atrribute: d:v
-        @param atrributes      : []
+        @param target_atrribute: algun valor presente en el dataset
+        @param atrributes      : conjunto de atributos a evaluar  
         '''
         def mas_comun(self,target_attribute):
         # Determina cual es el valor mas comun para el atributo objetivo
             polaridades = list(self.values[target_attribute])
             return max(set(polaridades), key=polaridades.count)
-
+        
         # Si todos los ejemplos tiene la misma polaridad, returna nodo con esa polaridad
         if len(set(self.values[target_attribute]))==1:
             return Nodo(self.values[target_attribute][0])
-
+        
         # Si no hay mas atributos, retorna el mas comun
         if not attributes:
             return Nodo(mas_comun(self,target_attribute))
-
+        
         # En otro caso
-        def mejor_clasifica(atts,tatt):
+        def mejor_clasifica(As,tA):            
             def information_gain(A,tA,S):
                 def subS(v,S,A):
                     # Calcula subconjunto de S donde el atributo A tiene valor v
+                    print A, v, [s for s in S if s[A]==v]
                     return [s for s in S if s[A]==v]
-                def entropy(S,target_attribute):
+                def entropy(S,tA):                    
                     def subclases(S):
-                        ordenado = sorted(S, key=lambda s : s[target_attribute])
-                        agrupado = groupby(ordenado, key=lambda s : s[target_attribute])
+                        ordenado = sorted(S, key=lambda s : s[tA])
+                        agrupado = groupby(ordenado, key=lambda s : s[tA])
                         # Obtiene las sublclases de S segun su polaridad
                         return [[y for y in list(x[1])] for x in agrupado]
                     # Calcula la entropia de S a partir de las subclases
+#                     print subclases(S)
                     return sum(-(1.0*len(subclass)/len(S))*log((1.0*len(subclass)/len(S)),2) for subclass in subclases(S))
                 # Calcula Information Gain a partir de la entropia y las sublcases
+#                 print '\n'.join([str(A)+"="+str(entropy(S,A)) for att in self.values.keys()])
+#                 print '\n'.join([str(A)+"="+str(entropy(S,A) - sum(entropy(subS(v,S,A),att) * len(subS(v,S,A))/len(S) for v in self.values[A])) for att in self.values.keys()])
+                print "------"
                 return entropy(S,tA) - sum(entropy(subS(v,S,A),tA) * len(subS(v,S,A))/len(S) for v in self.values[A])
             # Determina cual es el mejor atributo que clasifica a los ejemplos segun el Information Gain
-            return max(atts, key=lambda att : information_gain(att,tatt,self.examples))
-
+            return max(As, key=lambda A : information_gain(A,tA,self.examples))
+            
         A = mejor_clasifica(attributes,target_attribute)
         raiz = Nodo(A)
         for value in self.values[A]: # Buscar hijos
@@ -109,13 +111,13 @@ class ID3:
             else:
                 attributes.pop(A,None)
                 hijo = self.execute(target_attribute, attributes)
-            raiz.add_hijo(hijo)
-
+            raiz.add_hijo(hijo,value)                
+        
         return raiz
-
-    def run(self):
+    
+    def train(self):
         self.tree = self.execute()
-
+    
     def evaluate(self,instancia):
         pass
 
@@ -123,7 +125,7 @@ class ID3:
 learn = ID3()
 
 target_attribute = "JugarTenis"
-
 attributes = {"Cielo":"Lluvia", "Temperatura":"Suave", "Humedad":"Alta"  , "Viento":"Fuerte"}
-arbol = learn.execute("JugarTenis", attributes)
-print 6
+
+arbol = learn.execute(target_attribute, attributes)
+print arbol
